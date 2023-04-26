1003491279cb52ba8dc7b6145ee1fd811b61329e
Tyler Eppley <teppley300@gmail.com>
2023-04-26 19:52:16 -0400

    added post, read, and write file to server.js

--- server.js
+++ server.js
@@ -26,3 +26,42 @@ fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
     res.json(JSON.parse(notes))
 })
 });
+
+//use POST method to bring user input to backend
+app.post("/api/notes", (req, res) => {
+  //declare const for the note currently being saved by user
+  const currentNote = req.body;
+  //retrieve notes from db.json, get id of last note, add 1 to it to create 
+  //new id, save current note with new id
+fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
+    if (error) {
+        return console.log(error)
+    }
+    notes = JSON.parse(notes)
+    //assign unique id to each new note depending on last id.
+    //if no items in notes array, assign id as 10
+    if (notes.length > 0) {
+    let lastId = notes[notes.length - 1].id
+    var id =  parseInt(lastId)+ 1
+    } else {
+      var id = 10;
+    }
+    //create new note object
+    let newNote = { 
+      title: currentNote.title, 
+      text: currentNote.text, 
+      id: id 
+      }
+    //merge new note with existing notes array
+    var newNotesArr = notes.concat(newNote)
+    //write new array to db.json file and retuern it to user
+    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNotesArr), (error, data) => {
+      if (error) {
+        return error
+      }
+      console.log(newNotesArr)
+      res.json(newNotesArr);
+    })
+});
+
+});
