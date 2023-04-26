46ed6ad64b81c08542a16da399cbb3edea6b1b40
Tyler Eppley <teppley300@gmail.com>
2023-04-26 19:56:52 -0400

    added comments and space between comments

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //require all dependencies
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //direct user to correct page depending on url
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"))
 });
@@ -18,6 +20,7 @@ app.get("/notes", (req, res) => {
 });
 
 //send json of all notes if user accesses /api/notes
+
 app.get("/api/notes", (req, res) => {
 fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
     if (error) {
@@ -28,33 +31,45 @@ fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
 });
 
 //use POST method to bring user input to backend
+
 app.post("/api/notes", (req, res) => {
   //declare const for the note currently being saved by user
+
   const currentNote = req.body;
+
   //retrieve notes from db.json, get id of last note, add 1 to it to create 
   //new id, save current note with new id
+
 fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
     if (error) {
         return console.log(error)
     }
     notes = JSON.parse(notes)
+
     //assign unique id to each new note depending on last id.
     //if no items in notes array, assign id as 10
+
     if (notes.length > 0) {
     let lastId = notes[notes.length - 1].id
     var id =  parseInt(lastId)+ 1
     } else {
       var id = 10;
     }
+
     //create new note object
+
     let newNote = { 
       title: currentNote.title, 
       text: currentNote.text, 
       id: id 
       }
+
     //merge new note with existing notes array
+
     var newNotesArr = notes.concat(newNote)
+
     //write new array to db.json file and retuern it to user
+    
     fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNotesArr), (error, data) => {
       if (error) {
         return error
@@ -62,6 +77,5 @@ fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
       console.log(newNotesArr)
       res.json(newNotesArr);
     })
-});
-
+  });
 });
