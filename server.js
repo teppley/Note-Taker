4626726a47f4e9b7a705668d21ea3d0699c44ddc
Tyler Eppley <teppley300@gmail.com>
2023-04-26 19:49:06 -0400

    added read and send file to server.js

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//direct user to correct page depending on url
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+app.get("/notes", (req, res) => {
+ res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
+//send json of all notes if user accesses /api/notes
+app.get("/api/notes", (req, res) => {
+fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
+    if (error) {
+        return console.log(error)
+    }
+    res.json(JSON.parse(notes))
+})
+});
